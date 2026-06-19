b0aa993c882a0229461e4c3c96d58a315c41db89
Noel Escobedo <1341803+noelrock333@users.noreply.github.com>
2026-06-19 16:53:03 -0600

    refactor: restructure styles and update CSS to SCSS, migrate to Tailwind CSS for improved styling and organization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './index.css'
+import './styles/tailwind.css'
+import './styles/index.scss'
 import App from './App.jsx'
 
 const queryClient = new QueryClient({
